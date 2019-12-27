e053a4a7e1f93330d56dc8458975c55f61086206
August F.Y. Chao <AugustChao@narlabs.org.tw>
2019-12-27 11:27:58 +0800

    update personal usage

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -17,6 +17,12 @@ endtry
 set rtp+=~/tabnine-vim
 ' > ~/.vimrc
 
+echo '# AUGUST GIT INFO
+git config --global user.email "AugustChao@narlabs.org.tw"
+git config --global user.name "August F.Y. Chao"
+git config credential.helper cache
+' >> ~/.bashrc
+
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
 
 git clone --depth 1 https://github.com/zxqfl/tabnine-vim ~/.vim_runtime/my_plugins/tabnine-vim
---
+++ tools/TWCC_VM/sys_init.sh
@@ -0,0 +1,20 @@
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt update
+sudo apt -y install build-essential \
+    python2.7 python2.7-dev \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common \
+    docker-ce docker-ce-cli containerd.io
+
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.25.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo service docker restart
